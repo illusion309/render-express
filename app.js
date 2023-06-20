b04c30b2db17a389651ec82eb4eefdeb8f18d07a
illusion309 <sirmuko@gmail.com>
2023-06-20 11:56:14 +0200

    commit 1

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res
 .status(200)
 .render('index.html')
 )
+
+app.listen(port, ()=> {
+    console.log("alive");
+})
